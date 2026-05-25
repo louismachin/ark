5f86750906203927b0dfcff11f1df7c2e2340dc5
louismachin <louis@machin.dev>
2026-05-25 16:35:55 +0100

    Added search button

--- routes/index.rb
+++ routes/index.rb
@@ -3,3 +3,11 @@ get '/' do
     @iotas = Array.new(10) { Iota.new }
     erb :index
 end
+
+get '/search' do
+    query = params['q']
+    puts "query=#{query}"
+    @collections = Array.new(3) { Collection.new }
+    @iotas = Array.new(10) { Iota.new }
+    erb :index
+end
