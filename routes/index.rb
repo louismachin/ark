ed880e2d07351f08003ab375b9998f3989cde835
louismachin <louis@machin.dev>
2026-07-05 20:52:07 +0100

    Added collection form

--- routes/index.rb
+++ routes/index.rb
@@ -14,6 +14,12 @@ get '/iotas/new' do
     erb :iota_edit
 end
 
+get '/collections/new' do
+    @collections = get_collections
+    @iotas = get_iotas
+    erb :collection_edit
+end
+
 get '/search' do
     query = params['q']
     puts "query=#{query}"
@@ -54,5 +60,35 @@ post '/iotas/new' do
     # TODO: handle iota_params[:attachment] (a Sinatra file upload hash
     #       with :tempfile, :filename, :type) if present
  
+    redirect '/'
+end
+
+post '/collections/new' do
+    collection_params = params[:collection] || {}
+ 
+    tags = (collection_params[:tags] || '').split(',').map(&:strip).reject(&:empty?)
+ 
+    keys = Array(collection_params[:metadata_keys])
+    values = Array(collection_params[:metadata_values])
+    metadata = keys.zip(values).each_with_object({}) do |(k, v), hash|
+        next if k.nil? || k.strip.empty?
+        hash[k.strip] = v.to_s.strip
+    end
+
+    created_at = collection_params[:created_at]
+    created_at = Date.today.to_s if created_at == nil || created_at == ''
+ 
+    collection = Collection.new(
+        title:       collection_params[:title],
+        description: collection_params[:description],
+        tags:        tags,
+        created_at:  created_at,
+        metadata:    metadata
+    )
+
+    collection.save
+
+    puts collection.inspect
+
     redirect '/'
 end
