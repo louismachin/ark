b5a634b2bad815bd3a7b48710545b2f075d32356
louismachin <louis@machin.dev>
2026-07-05 20:05:55 +0100

    Normalized constants

--- helpers/iotas.rb
+++ helpers/iotas.rb
@@ -3,9 +3,9 @@ $iota_cache = nil
 def get_iotas
     return $iota_cache if $iota_cache
 
-    FileUtils.mkdir_p(Iota::IOTA_DIR)
+    FileUtils.mkdir_p(Iota::IOTA_PATH)
 
-    $iota_cache = Dir.glob(File.join(Iota::IOTA_DIR, '*.yml')).map do |file|
+    $iota_cache = Dir.glob(File.join(Iota::IOTA_PATH, '*.yml')).map do |file|
         Iota.from_hash(YAML.load_file(file))
     end
 end
