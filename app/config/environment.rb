69885c6a87320524b809b6ff2d765cc140a1b586
louismachin <louis@machin.dev>
2026-07-06 23:45:31 +0100

    Simple user sessions

--- app/config/environment.rb
+++ app/config/environment.rb
@@ -1,8 +1,10 @@
-APP_TITLE = 'Ark'
+APP_PORT     = '9393'
+APP_COOKIE   = '_ark'
+APP_TITLE    = 'Ark'
 APP_SUBTITLE = 'personal archive'
-APP_PORT = '9393'
-APP_ROOT = File.expand_path('..', __dir__)
-DATA_PATH = File.join(APP_ROOT, '..', 'data')
-VIEWS_PATH = File.join(APP_ROOT, 'views')
-PUBLIC_PATH = File.join(APP_ROOT, 'public')
-GITHUB_REPO = 'https://github.com/louismachin/ark'
+GITHUB_REPO  = 'https://github.com/louismachin/ark'
+APP_ROOT     = File.expand_path('..', __dir__)
+VIEWS_PATH   = File.join(APP_ROOT, 'views')
+PUBLIC_PATH  = File.join(APP_ROOT, 'public')
+DATA_PATH    = File.join(APP_ROOT, '..', 'data')
+USERS_PATH   = File.join(APP_ROOT, '..', '.users')
--- app/routes/session.rb
+++ app/routes/session.rb
@@ -1,5 +1,60 @@
+User = Struct.new(:username, :password)
+
+def get_users
+    result = {}
+    File.readlines(USERS_PATH).each { |line|
+        username, password = line.chomp.split(' ', 2)
+        result[username] = User.new(username, password)
+    }
+    return result
+end
+
+helpers do
+    def protect!
+        token = request.cookies[APP_COOKIE]
+        return $known_cookies.key?(token)
+    end
+
+    def current_user
+        return nil unless protect!
+        token = request.cookies[APP_COOKIE]
+        $known_cookies[token][:user]
+    rescue
+        return nil
+    end
+
+    def set_cookie(username)
+        token = SecureRandom.hex(8)
+        user = get_users[username]
+        puts "set_cookie\tuser=#{user}"
+        $known_cookies[token] = { user: user, time: Time.now }
+        response.set_cookie(APP_COOKIE, value: token, path: '/', max_age: '3600')
+    end
+
+    def unset_cookie
+        token = request.cookies[APP_COOKIE]
+        $known_cookies.delete(token)
+        response.delete_cookie(APP_COOKIE, path: '/')
+    end
+
+    def valid_user?(username, password)
+        puts "valid_user?\tusername=#{username}\tpassword=#{password}"
+        users = get_users
+        return false unless users.key?(username)
+        return users[username].password == password
+    end
+end
+
+$known_cookies = {}
+
+puts "Users:", get_users.keys
+
 get '/login' do
-    erb :login
+    if current_user
+        redirect '/'
+    else
+        erb :login
+    end
 end
 
 post '/login' do
@@ -7,8 +62,16 @@ post '/login' do
  
     username = login_params[:username]
     password = login_params[:password]
+    
+    if valid_user?(username, password)
+        set_cookie(username)
+        redirect '/'
+    else
+        redirect '/login'
+    end
+end
 
-    puts username, password
-
+get '/logout' do
+    unset_cookie
     redirect '/'
 end
