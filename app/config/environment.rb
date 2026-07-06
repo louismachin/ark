7dc6e070f3b9580625bd674f288a248137670d4d
louismachin <louis@machin.dev>
2026-07-06 22:56:11 +0100

    Added GitHub link to footer

--- app/config/environment.rb
+++ app/config/environment.rb
@@ -5,3 +5,4 @@ APP_ROOT = File.expand_path('..', __dir__)
 DATA_PATH = File.join(APP_ROOT, '..', 'data')
 VIEWS_PATH = File.join(APP_ROOT, 'views')
 PUBLIC_PATH = File.join(APP_ROOT, 'public')
+GITHUB_REPO = 'https://github.com/louismachin/ark'
