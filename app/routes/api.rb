a9cd5c3c3ae3f1b52d801c60e3cf77460ed0b518
louismachin <louis@machin.dev>
2026-07-06 01:28:04 +0100

    Refactored API

--- app/routes/api.rb
+++ app/routes/api.rb
@@ -4,14 +4,10 @@ get '/api/iotas.json' do
 end
 
 get '/api/iotas/:iota_id.json' do
-    content_type :json
     iota = find_iota_by_id(params[:iota_id])
-    if iota
-        iota.to_hash.to_json
-    else
-        status 404
-        nil
-    end
+    halt 404 unless iota
+    content_type :json
+    iota.to_hash.to_json
 end
 
 get '/api/collections.json' do
@@ -20,12 +16,8 @@ get '/api/collections.json' do
 end
 
 get '/api/collections/:collection_id.json' do
-    content_type :json
     collection = find_collection_by_id(params[:collection_id])
-    if collection
-        collection.to_hash.to_json
-    else
-        status 404
-        nil
-    end
+    halt 404 unless collection
+    content_type :json
+    collection.to_hash.to_json
 end
